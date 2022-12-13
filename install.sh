72ace09c7a02e22fdb848bf268bb7138a3eb827b
seek <seek@ntr0py.org>
2022-12-12 20:23:13 -0800

    Improved error checking

--- install.sh
+++ install.sh
@@ -6,36 +6,63 @@ alsa_ucm2_config_dir="/usr/share/alsa/ucm2/conf.d/"
 
 # Make sure ucm2 directory exists
 if [ ! -d "${alsa_ucm2_dir}" ]; then
-    printf "%s" "Directory ${alsa_ucm2_dir} does not exist."
-    printf "%s" "Aborting."
+    printf "%s\n" "Directory ${alsa_ucm2_dir} does not exist."
+    printf "%s\n" "Aborting."
     exit 1
 fi
 
 # Make sure ucm2 config dir exists
 if [ ! -d "${alsa_ucm2_config_dir}" ]; then
-    printf "%s" "Directory ${alsa_ucm2_config_dir} does not exist."
-    printf "%s" "Aborting."
+    printf "%s\n" "Directory ${alsa_ucm2_config_dir} does not exist."
+    printf "%s\n" "Aborting."
     exit 2
 fi
 
 # Make sure src config dir exists relative to cwd
 if [ ! -d "${src_config_dir}" ]; then
-    printf "%s" "Directory ${src_config_dir} does not exist."
-    printf "%s" "Aborting."
+    printf "%s\n" "Directory ${src_config_dir} does not exist."
+    printf "%s\n" "Aborting."
     exit 3
 fi
 
 # Create directories
-sudo mkdir -p "${alsa_ucm2_dir}/AMD/acp5x/"
-sudo mkdir -p "${alsa_ucm2_config_dir}/acp5x/"
+if ! mkdir -p "${alsa_ucm2_dir}/AMD/acp5x/"; then
+    printf "%s\n" "Failed to create directory."
+    printf "%s\n" "Aborting."
+    exit 4
+fi
+
+if ! mkdir -p "${alsa_ucm2_config_dir}/acp5x/"; then
+    printf "%s\n" "Failed to create directory."
+    printf "%s\n" "Aborting."
+    exit 5
+fi
 
 # Copy config files into appropriate directory
-sudo cp "${src_config_dir}/acp5x.conf" "${alsa_ucm2_dir}/AMD/acp5x/"
-sudo cp "${src_config_dir}/HiFi.conf" "${alsa_ucm2_dir}/AMD/acp5x/"
+if ! cp "${src_config_dir}/acp5x.conf" "${alsa_ucm2_dir}/AMD/acp5x/"; then
+    printf "%s\n" "Failed to copy file: acp5x.conf."
+    printf "%s\n" "Aborting."
+    exit 6
+fi
+
+if ! cp "${src_config_dir}/HiFi.conf" "${alsa_ucm2_dir}/AMD/acp5x/"; then
+    printf "%s\n" "Failed copy file: HiFi.conf"
+    printf "%s\n" "Aborting."
+    exit 7
+fi
 
 # Create symlink
-cd "${alsa_ucm2_config_dir}/acp5x/"
-sudo ln -s "../../AMD/acp5x/acp5x.conf"
+if ! cd "${alsa_ucm2_config_dir}/acp5x/"; then
+    printf "%s\n" "Failed change directory"
+    printf "%s\n" "Aborting."
+    exit 8
+fi
+
+if ! ln -s "../../AMD/acp5x/acp5x.conf"; then
+    printf "%s\n" "Failed create symlink."
+    exit 9
+fi
+
 
 # Done
 printf "%s" "Done."
